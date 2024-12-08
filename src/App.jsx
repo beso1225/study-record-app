a753b17e25a1c122f135de2ead392834b2c8af0d
beso1225 <yutaroformisc@gmail.com>
2024-12-08 08:13:27 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Helmet } from 'react-helmet-async'
+import './App.css'
+
+export const App = () => {
+
+  return (
+    <>
+      <Helmet>
+        <title>学習記録一覧</title>
+      </Helmet>
+    </>
+  )
+}
